25c6f08fca0cca054d3f7da81aad29f221c6735c
Rohan Hangal <rohanhangal@gmail.com>
2023-06-25 16:51:12 -0700

    created example booking

--- api/models/Place.js
+++ api/models/Place.js
@@ -9,8 +9,8 @@ const placeSchema = new Schema({
   description: String,
   perks: [String],
   extraInfo: String,
-  checkIn: Number,
-  checkOut: Number,
+  checkIn: String,
+  checkOut: String,
   maxGuests: Number,
   price: Number,
 });
